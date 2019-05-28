6fc1a6e8494b3b2a889bc3c2e5930231405be884
cumant <49497441+cumantsimon@users.noreply.github.com>
2019-05-28 14:50:09 -0700

    Update CreateWebJob.ps1
    modified on May 28 2019

--- CreateWebJob.ps1
+++ CreateWebJob.ps1
@@ -1,6 +1,8 @@
 
 $ScriptDir  = Set-Location -Path  "C:\Users\bcumant\Documents\Scripts"   -PassThru
 
+ 
+ 
 $file ="\WebJobCreate.json"
 
 $JsonObject   = Get-Content -Path .\$file |ConvertFrom-Json
@@ -12,27 +14,27 @@ Write-Host "Attributes individually printed"
 
 $resourceGroupName  = $JsonObject.deployment[0].resourceGroupName 
 $webAppName = $JsonObject.deployment[0].webAppName
-$Location =  $JsonObject.deployment[0].Location 
+$Location =  $JsonObject.deployment[0].location
 $Apiversion = $JsonObject.deployment[0].Apiversion
 $fileToUpload=$JsonObject.deployment[0].fileToUpload
 $WebjobName=$JsonObject.deployment[0].'$WebjobName'
 $WebjobType=$JsonObject.deployment[0].'$WebjobType'
+
+
+
 $SubscriptionId =$JsonObject.deployment[1].SubscriptionId
 $TenantId = $JsonObject.deployment[2].TenantId
 
 
 
 Login-AzureRmAccount –SubscriptionId $SubscriptionId -TenantId $TenantId
+
+
+ Select-AzureRMSubscription   -SubscriptionId $SubscriptionId 
+ 
 $context = Set-AzureRmContext -TenantId $TenantId -Name B2C -Force
 
 
-#Resource details :
-# $resourceGroupName = "az-prd-westus2-b2c-devauth";
-# $webAppName = "B2CdemoWebApp";
-# $Apiversion = 2015-08-01
-# $fileToUpload = "WebJob.zip"
-# $WebjobName="webjobtest"
-# $WebjobType="Continuous"
 
 
 #Function to get Publishing credentials for the WebApp :
@@ -50,12 +52,11 @@ $publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resour
 }
 
 
-# $creds = Get-PublishingProfileCredentials -resourceGroupName $resourceGroupName -webAppName $webAppName
+$creds = Get-PublishingProfileCredentials -resourceGroupName $resourceGroupName -webAppName $webAppName
 
 #Pulling authorization access token :
 
 function Get-KuduApiAuthorisationHeaderValue($resourceGroupName, $webAppName){
-
 
 
 $publishingCredentials = Get-PublishingProfileCredentials $resourceGroupName $webAppName
@@ -69,24 +70,40 @@ return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes
 $accessToken = Get-KuduApiAuthorisationHeaderValue $resourceGroupName $webAppname
 
 
-# to get site connection string and profiles 
+
 $PublishingProfile=Get-AzureRmWebAppSlotPublishingProfile -ResourceGroupName $resourceGroupName -Name $webAppname -Slot "staging" -Format WebDeploy  -OutputFile $ScriptDir\publishProfile.txt
 
 
-$Header = @{'Content-Disposition'='attachment; attachment; filename =$ScriptDir\$fileToUpload'
+
+# $Header = @{'Content-Disposition'='attachment; attachment; filename =$ScriptDir\$fileToUpload'
+#'Authorization'=$accessToken
+#    }
+# $apiUrl2="https://$webAppName.scm.azurewebsites.net/"
+
+
+
+
+$Header = @{'Content-Disposition'='INLINE; filename =WebJob.zip'
+
 
 'Authorization'=$accessToken
         }
 
+$URL="https://$webAppName.scm.azurewebsites.net/jobs/$WebjobType/$WebjobName/";
 
-$apiUrl2="https://$webAppName.scm.azurewebsites.net"
+
 $userAgent = "powershell/1.0"
+
 
 
 try {
 
 
-$response  = Invoke-RestMethod -Uri $apiUrl2 -Headers $Header -UserAgent $userAgent -Method POST -InFile $ScriptDir\$fileToUpload -ContentType "multipart/form-data"
+$response= Invoke-RestMethod -Uri $URL -Headers $Header  -Method Put -InFile $ScriptDir\$fileToUpload -ContentType "application/zip"
+
+
+
+#  $response  = Invoke-RestMethod -Uri $apiUrl2 -Headers $Header -UserAgent $userAgent -Method POST -InFile $ScriptDir\$fileToUpload -ContentType "multipart/form-data"
 
 
 Write-Host `n`n$response`n`n
@@ -99,19 +116,33 @@ Write-Host `n`n$response`n`n
     Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
     Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
 
+
     
 }
 
 
-# Start kudu  to verified deployment 
-Start-Process https://b2cdemowebapp-staging.scm.azurewebsites.net:443
-
-# for put method  if needed 
-# https://$webAppName-stage.scm.azurewebsites.net/api/$WebjobType/$WebjobName/run
 
 
 #Get list of all Webjobs
-Get-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceName $webAppName -ResourceType microsoft.web/sites/ -ApiVersion $Apiversion 
 
-#Start/Stop the Webjobs 
-# Invoke-AzureRmResourceAction -ApiVersion $Apiversion  -ResourceGroupName $ResourceGroupName  -ResourceType microsoft.web/sites/ -Action start  -Force 
+
+
+$WebJobs= Get-AzureRmResource  -ResourceGroup $ResourceGroupName -ResourceType microsoft.web/sites/continuouswebjobs -ResourceName $webAppName/$WebjobName -ApiVersion 2016-08-01
+
+
+
+#Start/Stop the Webjobs
+
+
+Invoke-AzureRmResourceAction  -ResourceGroup $ResourceGroupName  -ResourceType microsoft.web/sites/continuouswebjobs  -ResourceName $webAppName/$WebjobName -Action start  -ApiVersion 2016-08-01  -Force
+
+
+
+
+
+
+
+
+
+
+    
